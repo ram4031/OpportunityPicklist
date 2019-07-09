b3c25c05c6e20a9e33f590148050a7476a4114d4
ram4031 <ram4031@gmail.com>
2019-07-09 12:50:11 +0100

    Add row functionality

--- LWCDemo/force-app/main/default/aura/ManuMeet/ManuMeetController.js
+++ LWCDemo/force-app/main/default/aura/ManuMeet/ManuMeetController.js
@@ -2,8 +2,12 @@
     doinit: function (component, event, helper) {
         helper.getAccountlist(component);
     },
-    doSomething:function(component, event, helper)
-    {
-         helper.getoppslist(component);
-    }
+    doSomething: function (component, event, helper) {
+        helper.getoppslist(component);
+    },
+    // function for create new object Row in Contact List 
+    addNewRow: function (component, event, helper) {
+        // call the comman "createObjectData" helper method for add new Object Row to List  
+        helper.createObjectData(component, event);
+    },
 })
